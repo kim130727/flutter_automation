1b91d0fc1ca9bd38c92b5973546df3fe66643a38
kim130727 <kim130727@gmail.com>
2023-07-07 19:49:19 +0900

    0003

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:path/path.dart';
 import 'package:excel/excel.dart';
 import 'package:docx_template/docx_template.dart';
 
@@ -7,7 +6,7 @@ import 'package:docx_template/docx_template.dart';
 /// Read file template.docx, produce it and save
 ///
 void main() async {
-  final f = File("lib/template2.docx");
+  final f = File("lib/template.docx");
   final docx = await DocxTemplate.fromBytes(await f.readAsBytes());
 
   int num = 2;
@@ -19,92 +18,97 @@ void main() async {
     var sheet = excel['Sheet1'];
 
     String str = num.toString();
-    print(str);
     var cell0001 = sheet.cell(CellIndex.indexByString('A$str'));
-    print(cell0001.value);
-    print(cell0001.value.runtimeType);
+    var cell0002 = sheet.cell(CellIndex.indexByString('B$str'));
+    var cell0003 = sheet.cell(CellIndex.indexByString('C$str'));
+    var cell0004 = sheet.cell(CellIndex.indexByString('D$str'));
+    var cell0005 = sheet.cell(CellIndex.indexByString('E$str'));
+    var cell0006 = sheet.cell(CellIndex.indexByString('F$str'));
+    var cell0007 = sheet.cell(CellIndex.indexByString('G$str'));
+    var cell0008 = sheet.cell(CellIndex.indexByString('H$str'));
+    var cell0009 = sheet.cell(CellIndex.indexByString('I$str'));
+    var cell0010 = sheet.cell(CellIndex.indexByString('J$str'));
+    var cell0011 = sheet.cell(CellIndex.indexByString('K$str'));
+    var cell0012 = sheet.cell(CellIndex.indexByString('L$str'));
+    var cell0013 = sheet.cell(CellIndex.indexByString('M$str'));
+    var cell0014 = sheet.cell(CellIndex.indexByString('N$str'));
+    var cell0015 = sheet.cell(CellIndex.indexByString('O$str'));
+    var cell0016 = sheet.cell(CellIndex.indexByString('P$str'));
+    var cell0017 = sheet.cell(CellIndex.indexByString('Q$str'));
+    var cell0018 = sheet.cell(CellIndex.indexByString('R$str'));
+    var cell0019 = sheet.cell(CellIndex.indexByString('S$str'));
+    var cell0020 = sheet.cell(CellIndex.indexByString('T$str'));
+    var cell0021 = sheet.cell(CellIndex.indexByString('U$str'));
+    var cell0022 = sheet.cell(CellIndex.indexByString('V$str'));
+    var cell0023 = sheet.cell(CellIndex.indexByString('W$str'));
+    var cell0024 = sheet.cell(CellIndex.indexByString('X$str'));
+    var cell0025 = sheet.cell(CellIndex.indexByString('Y$str'));
+    var cell0026 = sheet.cell(CellIndex.indexByString('Z$str'));
+    var cell0027 = sheet.cell(CellIndex.indexByString('AA$str'));
 
     String text0001 = cell0001.value.toString();
-    String text0002 = "Thomas Jefferson's knowledge";
-    String text0003 = "reconnaissance";
+    String text0002 = cell0002.value.toString();
+    String text0003 = cell0003.value.toString();
     if (text0003 != "") {
       text0003 = "*$text0003";
     }
-    String text0004 = "정찰";
-    String text0005 = "strain";
+    String text0004 = cell0004.value.toString();
+    if (text0004 != "") {
+      text0004 = "***$text0004";
+    }
+    String text0005 = cell0005.value.toString();
     if (text0005 != "") {
       text0005 = "**$text0005";
     }
-    String text0006 = "품종";
-    String text0007 = "undaunted";
-    if (text0007 != "") {
-      text0007 = "***$text0007";
+    String text0006 = "[정답] ${cell0006.value}";
+    String text0007 = "[소재] ${cell0007.value}";
+    String text0008 = "[해석] ${cell0008.value}";
+    String text0009 = "[해설] ${cell0009.value}";
+    String text0010 = cell0010.value.toString();
+    if (text0010 != "") {
+      text0010 = "□$text0010";
     }
-    String text0008 = "굴하지 않는2";
-    String text0009 = "[정답]";
-    String text0010 = "5";
-    String text0011 = "[소재]";
-    String text0012 = "동화 작가이자 삽화가인 Leo Lionni";
-    String text0013 = "[해석]";
-    String text0014 = "국제적으로 알려진 디자이너이자 삽화가이자 그래픽 아티스트였던 ";
-    String text0015 = "[해설]";
-    String text0016 = "1982년, Lionni는 파킨슨병을 진단받았지만, ";
-    String text0017 = "[어휘]";
-    String text0018 = "illustrator";
+    String text0011 = cell0011.value.toString();
+    String text0012 = cell0012.value.toString();
+    if (text0012 != "") {
+      text0012 = "□$text0012";
+    }
+    String text0013 = cell0013.value.toString();
+    String text0014 = cell0014.value.toString();
+    if (text0014 != "") {
+      text0014 = "□$text0014";
+    }
+    String text0015 = cell0015.value.toString();
+    String text0016 = cell0016.value.toString();
+    if (text0016 != "") {
+      text0016 = "□$text0016";
+    }
+    String text0017 = cell0017.value.toString();
+    String text0018 = cell0018.value.toString();
     if (text0018 != "") {
       text0018 = "□$text0018";
     }
-    String text0019 = "삽화가";
-    String text0020 = "illustrator";
+    String text0019 = cell0019.value.toString();
+    String text0020 = cell0020.value.toString();
     if (text0020 != "") {
       text0020 = "□$text0020";
     }
-    String text0021 = "삽화가";
-    String text0022 = "illustrator";
+    String text0021 = cell0021.value.toString();
+    String text0022 = cell0022.value.toString();
     if (text0022 != "") {
       text0022 = "□$text0022";
     }
-    String text0023 = "삽화가";
-    String text0024 = "illustrator";
+    String text0023 = cell0023.value.toString();
+    String text0024 = cell0024.value.toString();
     if (text0024 != "") {
       text0024 = "□$text0024";
     }
-    String text0025 = "삽화가";
-    String text0026 = "illustrator";
+    String text0025 = cell0025.value.toString();
+    String text0026 = cell0026.value.toString();
     if (text0026 != "") {
       text0026 = "□$text0026";
     }
-    String text0027 = "삽화가";
-    String text0028 = "illustrator";
-    if (text0028 != "") {
-      text0028 = "□$text0028";
-    }
-    String text0029 = "삽화가";
-    String text0030 = "illustrator";
-    if (text0030 != "") {
-      text0030 = "□$text0030";
-    }
-    String text0031 = "삽화가";
-    String text0032 = "illustrator";
-    if (text0032 != "") {
-      text0032 = "□$text0032";
-    }
-    String text0033 = "삽화가";
-    String text0034 = "illustrator";
-    if (text0034 != "") {
-      text0034 = "□$text0034";
-    }
-    String text0035 = "삽화가";
-    String text0036 = "illustrator";
-    if (text0036 != "") {
-      text0036 = "□$text0036";
-    }
-    String text0037 = "삽화가";
-    String text0038 = "illustrator";
-    if (text0038 != "") {
-      text0038 = "□$text0038";
-    }
-    String text0039 = "삽화가";
+    String text0027 = cell0027.value.toString();
 
     Content content = Content();
     content
@@ -134,19 +138,7 @@ void main() async {
       ..add(TextContent("0024", text0024))
       ..add(TextContent("0025", text0025))
       ..add(TextContent("0026", text0026))
-      ..add(TextContent("0027", text0027))
-      ..add(TextContent("0028", text0028))
-      ..add(TextContent("0029", text0029))
-      ..add(TextContent("0030", text0030))
-      ..add(TextContent("0031", text0031))
-      ..add(TextContent("0032", text0032))
-      ..add(TextContent("0033", text0033))
-      ..add(TextContent("0034", text0034))
-      ..add(TextContent("0035", text0035))
-      ..add(TextContent("0036", text0036))
-      ..add(TextContent("0037", text0037))
-      ..add(TextContent("0038", text0038))
-      ..add(TextContent("0039", text0039));
+      ..add(TextContent("0027", text0027));
 
     final docGenerated = await docx.generate(content);
     final fileGenerated = File('result/${str}generated.docx');
